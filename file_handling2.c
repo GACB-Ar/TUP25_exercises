5aadc4334ad1cb33810525399ea9dc15604cb44c
Cespedes Geronimo <gerocb.lab@gmail.com>
2025-06-17 07:20:34 -0300

    added cheatsheet for file handling

--- file_handling2.c
+++ file_handling2.c
@@ -7,6 +7,13 @@ int main()
 
     archive = fopen("names.txt", "w");
 
-    
+    /*
+        "r" -> Leer (el archivo debe existir)
+        "w" -> Escribir (crea el archivo o lo sobreescribe si ya existe)
+        "a" -> Agregar (append) al final del archivo
+        "r+" -> Leer y escribir (el archivo debe existir)
+        "w+" -> Leer y escribir (borra el contenido anterior)
+        "a+" -> Leer y agregar (no borra el contenido anterior)
+    */
     return 0;
 }
