2967e365e92e60a48b316fbea46ed1fcb42674a3
Cespedes Geronimo <gerocb.lab@gmail.com>
2025-06-17 07:16:17 -0300

    added professor repo

--- file_handling2.c
+++ file_handling2.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+void user_requests(int param, FILE *file_param);
 
 int main()
 {
-    /* code */
+    FILE *archive;
+
+    archive = fopen("names.txt", "w");
+
+    
     return 0;
 }
